54aba7955ebc68731b8c5f3af49e2fbce0517284
leah <theloadingpage@gmail.com>
2022-09-08 18:11:41 +0100

    added yarn start script

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ async function testPandaDatabase() {
     console.log("function start")
     const client = new Client({connectionString: process.env.PANDASECRET, ssl: {rejectUnauthorized: false}})
     await client.connect()
-    // const res = await client.query('select * from todos')
-    const res = await client.query("insert into todos (description, status) values('find friends', true)")
+    const res = await client.query('select * from todos')
+    // const res = await client.query("insert into todos (description, status) values('find friends', true)")
     console.log(res.rows)
     await client.end()
 }
