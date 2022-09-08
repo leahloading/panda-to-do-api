76a7a55c7aabf5d76cc394ca083c2e499af88755
leah <theloadingpage@gmail.com>
2022-09-08 17:50:59 +0100

    make connection to db

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const { Client } = require('pg')
+
 console.log("hello world");
+
+async function testPandaDatabase() { 
+
+    console.log("function start")
+    const client = new Client({connectionString: process.env.PANDASECRET, ssl: {rejectUnauthorized: false}})
+    await client.connect()
+    // const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    // console.log(res.rows[0].message) // Hello world!
+    // await client.end()
+}
+
+testPandaDatabase();
+console.log("end");
